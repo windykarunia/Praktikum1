e0bb887011e31a59431919801008bb53b0199af6
windykarunia <windykaruniasari26@gmail.com>
2022-12-26 23:00:00 +0700

    baru

---
+++ app/Http/Controllers/Tugas1Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\http\request;
+
+class Tugas1Controller extends Controllers
+{
+    public function index(){
+        return view('tugas1');
+    }
+}
---
+++ resources/views/tugas1.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+</head>
+<body><h2>Selamat datang di Tugas1</h2>
+        <p>Windy</p>
+        <p>G.231.21.0128</p>
+</body>
+</html>
